c3cfe1a326abb48a87e86291e874b96e9dcdb823
Jin <663689981@qq.com>
2019-10-10 16:12:55 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true,
+        "node": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:vue/essential",
+        "airbnb-base"
+    ],
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "vue"
+    ],
+    "rules":{
+      "semi":0,
+      "comma-dangle":0,
+      "no-tabs":0
+    }
+};
